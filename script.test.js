f387df6ab06d9fafbb52299993b09639bde5b5be
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-01 12:14:24 -0400

    Add tests for basic math operations

--- script.test.js
+++ script.test.js
@@ -14,3 +14,18 @@ test('reverses a string', () => {
     expect(reverseString('flamingo')).toBe('ognimalf')
     expect(reverseString('10Love')).toBe('evoL01')
 })
+
+describe('calculator', () => {
+    it('adds two numbers', () => {
+        expect(calculator.add(45, 15)).toEqual(60)
+    })
+    it('subtracts two numbers', () => {
+        expect(calculator.subtract(45, 15)).toEqual(30)
+    })
+    it('multiplys two numbers', () => {
+        expect(calculator.multiply(5, 9)).toEqual(45)
+    })
+    it('divides two numbers', () => {
+        expect(calculator.divide(45, 15)).toEqual(3)
+    })
+})
