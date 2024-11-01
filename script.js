013b2768a95a84a9c7c39ac9b82b66f4c9574f3d
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-01 12:07:43 -0400

    Add function to reverse a string

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function capitalize(string) {
+module.exports.capitalize = function(string) {
     let array = string.split('')
     array[0] = array[0].toUpperCase()
     return array.join('')
 }
 
-module.exports = capitalize;
+module.exports.reverseString = function(string) {
+    let array = string.split('')
+    let reversedArray = []
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversedArray.push(array[i])
+    }
+    return reversedArray.join('')
+}
