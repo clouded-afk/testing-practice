1224231e89f441e0d36e361fb319731e6d5fba4b
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-11-01 12:07:23 -0400

    Add test for reverseString

--- script.test.js
+++ script.test.js
@@ -1,7 +1,16 @@
-const capitalize = require('./script');
+const {
+    capitalize,
+    reverseString,
+    calculator,
+} = require('./script');
 
 test('capitalizes first character of a string', () => {
     expect(capitalize('stop')).toBe('Stop');
     expect(capitalize('Llama')).toBe('Llama');
     expect(capitalize('10Love')).toBe('10Love');
+})
+
+test('reverses a string', () => {
+    expect(reverseString('flamingo')).toBe('ognimalf')
+    expect(reverseString('10Love')).toBe('evoL01')
 })
